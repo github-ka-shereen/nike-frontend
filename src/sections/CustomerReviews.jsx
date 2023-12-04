a6ee3fe3d75685e329ed10a92b979bb5ce5b52ad
Shereni <info@aewebsites.co.zw>
2023-12-04 23:36:36 +0200

    removed unwanted files

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -8,7 +8,7 @@ const CustomerReviews = () => {
     What Our{' '}
     <span className="text-coral-red">Customers</span> Say?
     </h3>
-    <p className="info-text m-auto mt-4 max-w-lg text-center">Here genuine stories from our satisfied customers
+    <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers
     about their exceptional experience with us.</p>
     <div className="mt-24 flex flex-1 justify-evenly item-center max-lg:flex-col gap-14">
     {reviews.map((review) => (
